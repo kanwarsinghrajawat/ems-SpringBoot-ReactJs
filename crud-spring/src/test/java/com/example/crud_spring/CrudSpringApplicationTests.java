14b1431fba25bd6c21caa23a83f4b182764235eb
Kanwar Singh <kanwarsinghrajawat3@gmail.com>
2024-07-08 09:09:31 +0530

    testing

--- crud-spring/src/test/java/com/example/crud_spring/CrudSpringApplicationTests.java
+++ crud-spring/src/test/java/com/example/crud_spring/CrudSpringApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.crud_spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//
 @SpringBootTest
 class CrudSpringApplicationTests {
 
